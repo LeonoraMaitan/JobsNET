3c27963acf9f2f73a7b99c33c82c571dc508c509
Leonora Maitan <leonora.maitan@gmail.com>
2021-08-25 00:05:56 -0300

    Ajustes backend

---
+++ backend/README.md
@@ -0,0 +1,17 @@
+# JobsNet API
+
+## Requisitos
+Antes de começar certifique-se de instalar o [NodeJS](https://nodejs.org/en/download/) e [Mongo](https://docs.mongodb.com/manual/installation/)
+
+## Como executar
+Antes de iniciar a aplicação de backend é necessário instalar dependências com o comando abaixo:
+```
+npm install
+```
+
+Em seguida, para iniciar a aplicação, execute o comando:
+```
+node app.js
+``` 
+
+Acesse http://localhost:3000/docs
--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,24 @@
 const express = require('express')
+
 const app = express()
 const port = 3000
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://+:${port}`)
+    console.log(`JobsNet server listening at http://+:${port}`)
 })
 
-app.get('/test', (req, res) => {
-    res.send('Hello World!')
+app.get('/', (req, res) => {
+    res.send('JobsNet server is running')
+})
+
+app.post('/curriculos', (req, res) => {
+    try {
+        if (!req.body.nome) {
+            res.status(503)
+        }
+    }
+    catch {
+        res.status(500).send('Falha ocorrida. Por favor, tente novamente.')
+    }
+    
 })
